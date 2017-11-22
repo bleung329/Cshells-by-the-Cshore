c28476f05b9a6eb86f7d81e813e25c459c79570d
bleung329 <bleung@stuy.edu>
2017-11-22 15:35:47 -0500

    we are security professionals *uses gets

---
+++ shell.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+char** parse_args(char* yeah)
+{
+	//Init copy of argstring and array to store it.
+	char * yeahcpy = (char *)malloc(sizeof(yeah));
+	strcpy(yeahcpy,yeah);
+	char **arr = (char**) malloc(sizeof *arr * 8); 
+
+	if (arr)
+	{
+	  //The i is just to help me keep track of the strings.
+	  int i = 0;
+	  while(yeahcpy!=NULL)
+	  {
+	  	//Copy the strsepped string into the array.
+	    arr[i] = malloc(sizeof *arr[i] * 8);
+	    printf("yeahcpy is %s\n",yeahcpy);
+	    strcpy(arr[i], strsep(&yeahcpy," ")); 
+	    printf("arr[%d] is %s\n",i,arr[i]);
+	    i++;
+	  }
+	}
+	//Free the copied string.
+	free(yeahcpy);
+	return arr;
+
+}
+
+int cshell()
+{
+	char rawcmd[256];
+	gets(rawcmd,256,stdin);
+	char** cmd;
+	cmd = parse_args(rawcmd);
+	execvp(cmd[0],cmd);
+	printf(">");
+	return 0;
+
+}
+
+int main(int argc, char **argv)
+{
+	  // Load config files, if any.
+
+	  // Run command loop.
+
+	cshell();
+		
+
+
+	// Perform any shutdown/cleanup.
+
+	return 0;
+}
